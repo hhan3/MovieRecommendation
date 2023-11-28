8bbd400e68fd431df82a16ee66b16954462e6f43
haosen han <hhan18@hawk.iit.edu>
2023-11-27 20:37:01 -0600

    rating page finished  push 3rd

--- WebApplication1/Areas/HelpPage/Controllers/HelpController.cs
+++ WebApplication1/Areas/HelpPage/Controllers/HelpController.cs
@@ -3,6 +3,11 @@ using System.Web.Http;
 using System.Web.Mvc;
 using WebApplication1.Areas.HelpPage.ModelDescriptions;
 using WebApplication1.Areas.HelpPage.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+
 
 namespace WebApplication1.Areas.HelpPage.Controllers
 {
@@ -25,18 +30,20 @@ namespace WebApplication1.Areas.HelpPage.Controllers
 
         public HttpConfiguration Configuration { get; private set; }
 
+      
         public ActionResult Index()
         {
             ViewBag.DocumentationProvider = Configuration.Services.GetDocumentationProvider();
             return View(Configuration.Services.GetApiExplorer().ApiDescriptions);
         }
 
-        public ActionResult Contact()
+        
+        public ActionResult ApiList()
         {
             ViewBag.DocumentationProvider = Configuration.Services.GetDocumentationProvider();
             return View(Configuration.Services.GetApiExplorer().ApiDescriptions);
         }
-
+       
 
         public ActionResult Api(string apiId)
         {
@@ -52,6 +59,11 @@ namespace WebApplication1.Areas.HelpPage.Controllers
             return View(ErrorViewName);
         }
 
+
+
+
+
+
         public ActionResult ResourceModel(string modelName)
         {
             if (!String.IsNullOrEmpty(modelName))
@@ -66,5 +78,48 @@ namespace WebApplication1.Areas.HelpPage.Controllers
 
             return View(ErrorViewName);
         }
+
+
+        public async Task<ActionResult> CouponApi()
+
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://get-promo-codes.p.rapidapi.com/data/get-coupons/?page=1&sort=update_time_desc"),
+                Headers =
+        {
+            { "X-RapidAPI-Key", "f45a75ea1cmsh0dd1751d526138fp1f1ac1jsn1b2f727d5f7a" },
+            { "X-RapidAPI-Host", "get-promo-codes.p.rapidapi.com" },
+        },
+            };
+
+            try
+            {
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    ViewBag.CouponData = body; // Assuming the response is in a format that your view can handle
+                    return View("CouponApiView"); // Replace with the name of your Razor view
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions (log the error, return an error view, etc.)
+                return View(ErrorViewName); // Or a view that displays the error
+            }
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Net.Http; 
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using System.Web.Mvc.Html;
+
+
+
+
+
+
 
 namespace WebApplication1.Controllers
 {
@@ -32,18 +42,109 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-        public ActionResult Rating()
-        {
-            return View();
-        }
+      
         public ActionResult Review()
         {
             return View();
         }
 
-        public ActionResult Coupons()
+      /*
+
+        public ActionResult ResourceModel(string modelName)
         {
-            return View();
+            if (!String.IsNullOrEmpty(modelName))
+            {
+                ModelDescriptionGenerator modelDescriptionGenerator = Configuration.GetModelDescriptionGenerator();
+                ModelDescription modelDescription;
+                if (modelDescriptionGenerator.GeneratedModels.TryGetValue(modelName, out modelDescription))
+                {
+                    return View(modelDescription);
+                }
+            }
+
+            return View(ErrorViewName);
         }
-    }
+        
+        */
+
+        public async Task<ActionResult> Coupons()
+
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://get-promo-codes.p.rapidapi.com/data/get-coupons/?page=1&sort=update_time_desc"),
+                Headers =
+        {
+            { "X-RapidAPI-Key", "f45a75ea1cmsh0dd1751d526138fp1f1ac1jsn1b2f727d5f7a" },
+            { "X-RapidAPI-Host", "get-promo-codes.p.rapidapi.com" },
+        },
+            };
+
+            try
+            {
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    ViewBag.CouponData = body; // Assuming the response is in a format that your view can handle
+                    return View("Coupons"); 
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Failed to load coupons. Please try again later.";
+                return View("Error"); // An error view that shows the message
+
+              
+            }
+        }
+
+
+
+
+            public async Task<ActionResult> Rating(string id)
+            {
+
+
+            var movieId = string.IsNullOrEmpty(id) ? "defaultMovieId" : id;
+
+            var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://movies-ratings2.p.rapidapi.com/ratings?id={movieId}"),
+                    Headers =
+                {
+                    { "X-RapidAPI-Key", "f45a75ea1cmsh0dd1751d526138fp1f1ac1jsn1b2f727d5f7a" },
+                    { "X-RapidAPI-Host", "movies-ratings2.p.rapidapi.com" },
+
+                    
+                },
+                };
+
+                try
+                {
+                    using (var response = await client.SendAsync(request))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var body = await response.Content.ReadAsStringAsync();
+                        ViewBag.RatingData = body;
+                        return View("Rating"); 
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions (log the error, return an error view, etc.)
+                    ViewBag.ErrorMessage = "Failed to load rating data. Please try again later.";
+                    return View("Error"); 
+                }
+            }
+
+           
+
+
+
+}
 }
